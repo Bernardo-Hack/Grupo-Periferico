6d42c3af0dd86be0c3f586568cb6b15e457938e2
Ítalo de Andrade <italolande@gmail.com>
2025-05-10 19:58:21 -0300

    mysql modify

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,12 +3,23 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+const db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-
+  waitForConnections: true
 });
 
-export default pool;
+
+(async () => {
+  try {
+    const conn = await db.getConnection(); 
+    console.log(' MySQL pool conectado com sucesso!');
+    conn.release(); 
+  } catch (err) {
+    console.error('❌ Erro ao conectar ao MySQL:', (err as Error).message);
+  }
+})();
+
+export default db;
