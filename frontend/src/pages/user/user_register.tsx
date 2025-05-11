d2c62e95ab1dfe1f259310042b7f6faf9519ff9c
TheoRavaglia <theo.ravaglia@pucpr.edu.br>
2025-05-11 17:30:36 -0300

    better register page

--- frontend/src/pages/user/user_register.tsx
+++ frontend/src/pages/user/user_register.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { initMDB, Tab, Input, Ripple } from 'mdb-ui-kit';
+import { Navbar } from '../../layouts/shared/navbar';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import '../../layouts/style/user_registerCSS.css';
 
@@ -11,187 +12,171 @@ const Login: React.FC = () => {
             console.error('Erro ao inicializar o MDB UI Kit:', error);
         }
     }, []);
-
     return (
 <div className="login-page-wrapper d-flex flex-column min-vh-100">
+   <Navbar />
   <div className="d-flex flex-grow-1 align-items-center justify-content-center">
     <div className="login-register-container">
-      {/* Pills navs */}
-        <ul className="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
-          <li className="nav-item" role="presentation">
-            <a
-              className="nav-link active"
-              id="tab-login"
-              data-mdb-pill-init
-              href="#pills-login"
-              role="tab"
-              aria-controls="pills-login"
-              aria-selected="true"
-            >
-              Login
-            </a>
-          </li>
-          <li className="nav-item" role="presentation">
-            <a
-              className="nav-link"
-              id="tab-register"
-              data-mdb-pill-init
-              href="#pills-register"
-              role="tab"
-              aria-controls="pills-register"
-              aria-selected="false"
-            >
-              Register
-            </a>
-          </li>
-        </ul>
+      {/* Abas de navegação */}
+      <ul className="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
+        <li className="nav-item" role="presentation">
+          <a
+            className="nav-link active"
+            id="tab-login"
+            data-mdb-pill-init
+            href="#pills-login"
+            role="tab"
+            aria-controls="pills-login"
+            aria-selected="true"
+          >
+            Login
+          </a>
+        </li>
+        <li className="nav-item" role="presentation">
+          <a
+            className="nav-link"
+            id="tab-register"
+            data-mdb-pill-init
+            href="#pills-register"
+            role="tab"
+            aria-controls="pills-register"
+            aria-selected="false"
+          >
+            Cadastro
+          </a>
+        </li>
+      </ul>
 
-        {/* Pills content */}
-        <div className="tab-content">
-          <div
-            className="tab-pane fade show active"
-            id="pills-login"
-            role="tabpanel"
-            aria-labelledby="tab-login"
-          >
-            <form>
-              <div className="text-center mb-3">
-            <p>Sign in with:</p>
-            <button data-mdb-ripple-init type="button" className="btn btn-secondary btn-floating mx-1">
-              <i className="fab fa-facebook-f"></i>
-            </button>
-            <button data-mdb-ripple-init type="button" className="btn btn-secondary btn-floating mx-1">
-              <i className="fab fa-google"></i>
-            </button>
-            <button data-mdb-ripple-init type="button" className="btn btn-secondary btn-floating mx-1">
-              <i className="fab fa-twitter"></i>
-            </button>
-            <button data-mdb-ripple-init type="button" className="btn btn-secondary btn-floating mx-1">
-              <i className="fab fa-github"></i>
-            </button>
+      {/* Conteúdo das abas */}
+      <div className="tab-content">
+        <div
+          className="tab-pane fade show active"
+          id="pills-login"
+          role="tabpanel"
+          aria-labelledby="tab-login"
+        >
+          <form>
+            {/* Campo de email */}
+            <div data-mdb-input-init className="form-outline mb-4">
+              <input type="email" id="loginName" className="form-control" />
+              <label className="form-label" htmlFor="loginName">
+                Email ou nome de usuário
+              </label>
+            </div>
+            
+            {/* Campo de senha */}
+            <div data-mdb-input-init className="form-outline mb-4">
+              <input type="password" id="loginPassword" className="form-control" />
+              <label className="form-label" htmlFor="loginPassword">
+                Senha
+              </label>
+            </div>
+            
+            {/* Checkbox e link */}
+            <div className="row mb-4">
+              <div className="col-md-6 d-flex justify-content-center">
+                <div className="form-check mb-3 mb-md-0">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="loginCheck"
+                    defaultChecked
+                  />
+                  <label className="form-check-label" htmlFor="loginCheck">
+                    Lembrar de mim
+                  </label>
+                </div>
               </div>
-              <p className="text-center">or:</p>
-              {/* Email input */}
-              <div data-mdb-input-init className="form-outline mb-4">
-            <input type="email" id="loginName" className="form-control" />
-            <label className="form-label" htmlFor="loginName">
-              Email or username
-            </label>
-              </div>
-              {/* Password input */}
-              <div data-mdb-input-init className="form-outline mb-4">
-            <input type="password" id="loginPassword" className="form-control" />
-            <label className="form-label" htmlFor="loginPassword">
-              Password
-            </label>
-              </div>
-              {/* 2 column grid layout */}
-              <div className="row mb-4">
-            <div className="col-md-6 d-flex justify-content-center">
-              <div className="form-check mb-3 mb-md-0">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="loginCheck"
-              defaultChecked
-            />
-            <label className="form-check-label" htmlFor="loginCheck">
-              Remember me
-            </label>
+              <div className="col-md-6 d-flex justify-content-center">
+                <a href="#!">Esqueceu a senha?</a>
               </div>
             </div>
-            <div className="col-md-6 d-flex justify-content-center">
-              <a href="#!">Forgot password?</a>
+            
+            <button type="submit" className="btn btn-primary btn-block mb-4">
+              Entrar
+            </button>
+            
+            <div className="text-center">
+              <p>
+                Não é membro? <a href="#!">Cadastre-se</a>
+              </p>
             </div>
-              </div>
-              <button type="submit" className="btn btn-primary btn-block mb-4">
-            Sign in
-              </button>
-              <div className="text-center">
-            <p>
-              Not a member? <a href="#!">Register</a>
-            </p>
-              </div>
-            </form>
-          </div>
-          <div
-            className="tab-pane fade"
-            id="pills-register"
-            role="tabpanel"
-            aria-labelledby="tab-register"
-          >
-            <form className="register-form">
-              <div className="text-center mb-3">
-            <p>Sign up with:</p>
-            <button data-mdb-ripple-init type="button" className="btn btn-secondary btn-floating mx-1">
-              <i className="fab fa-facebook-f"></i>
-            </button>
-            <button data-mdb-ripple-init type="button" className="btn btn-secondary btn-floating mx-1">
-              <i className="fab fa-google"></i>
-            </button>
-            <button data-mdb-ripple-init type="button" className="btn btn-secondary btn-floating mx-1">
-              <i className="fab fa-twitter"></i>
-            </button>
-            <button data-mdb-ripple-init type="button" className="btn btn-secondary btn-floating mx-1">
-              <i className="fab fa-github"></i>
-            </button>
-              </div>
-              <p className="text-center">or:</p>
-              {/* Nome input */}
-              <div data-mdb-input-init className="form-outline mb-4">
-            <input type="text" id="registerName" className="form-control" />
-            <label className="form-label" htmlFor="registerName">Nome</label>
-              </div>
-              {/* usuario de login input */}
-              <div data-mdb-input-init className="form-outline mb-4">
-            <input type="text" id="registerUsername" className="form-control" />
-            <label className="form-label" htmlFor="registerUsername">usuario do login</label>
-              </div>
-              {/* CPF input */}
-              <div data-mdb-input-init className="form-outline mb-4">
-            <input type="email" id="registerEmail" className="form-control" />
-            <label className="form-label" htmlFor="registerEmail">CPF</label>
-              </div>
-              {/* telefone input */}
-              <div data-mdb-input-init className="form-outline mb-4">
-            <input type="text" id="registerEmail" className="form-control" />
-            <label className="form-label" htmlFor="registerEmail">Telefone</label>
-              </div>
-              {/*dt nascimento  input */}
-              <div data-mdb-input-init className="form-outline mb-4">
-            <input type="date" id="registerEmail" className="form-control" />
-            <label className="form-label" htmlFor="registerEmail">data de nascimento</label>
-              </div>
-              {/* Password input */}
-              <div data-mdb-input-init className="form-outline mb-4">
-            <input type="password" id="registerPassword" className="form-control" />
-            <label className="form-label" htmlFor="registerPassword">Password</label>
-              </div>
-              {/* Repeat Password input */}
-              <div data-mdb-input-init className="form-outline mb-4">
-            <input type="password" id="registerRepeatPassword" className="form-control" />
-            <label className="form-label" htmlFor="registerRepeatPassword">Repeat password</label>
-              </div>
-              <div className="form-check d-flex justify-content-center mb-4">
-            <input
-              className="form-check-input me-2"
-              type="checkbox"
-              value=""
-              id="registerCheck"
-              defaultChecked
-              aria-describedby="registerCheckHelpText"
-            />
-            <label className="form-check-label" htmlFor="registerCheck">
-              I have read and agree to the terms
-            </label>
-              </div>
-              <button type="submit" className="btn btn-primary btn-block mb-3">
-            cadastrar
-              </button>
-            </form>
-          </div>{/*div de registro*/}
+          </form>
         </div>
+        
+        {/* Aba de cadastro */}
+        <div
+          className="tab-pane fade"
+          id="pills-register"
+          role="tabpanel"
+          aria-labelledby="tab-register"
+        >
+          <form className="register-form">
+            
+            {/* Campo de nome */}
+            <div data-mdb-input-init className="form-outline mb-4">
+              <input type="text" id="registerName" className="form-control" />
+              <label className="form-label" htmlFor="registerName">Nome completo</label>
+            </div>
+            
+            {/* Campo de usuário */}
+            <div data-mdb-input-init className="form-outline mb-4">
+              <input type="text" id="registerUsername" className="form-control" />
+              <label className="form-label" htmlFor="registerUsername">Nome de usuário</label>
+            </div>
+            
+            {/* Campo de CPF */}
+            <div data-mdb-input-init className="form-outline mb-4">
+              <input type="text" id="registerEmail" className="form-control" />
+              <label className="form-label" htmlFor="registerEmail">CPF</label>
+            </div>
+            
+            {/* Campo de telefone */}
+            <div data-mdb-input-init className="form-outline mb-4">
+              <input type="text" id="registerEmail" className="form-control" />
+              <label className="form-label" htmlFor="registerEmail">Telefone</label>
+            </div>
+            
+            {/* Campo de data de nascimento */}
+            <div data-mdb-input-init className="form-outline mb-4">
+              <input type="date" id="registerEmail" className="form-control" />
+              <label className="form-label" htmlFor="registerEmail">Data de nascimento</label>
+            </div>
+            
+            {/* Campo de senha */}
+            <div data-mdb-input-init className="form-outline mb-4">
+              <input type="password" id="registerPassword" className="form-control" />
+              <label className="form-label" htmlFor="registerPassword">Senha</label>
+            </div>
+            
+            {/* Confirmação de senha */}
+            <div data-mdb-input-init className="form-outline mb-4">
+              <input type="password" id="registerRepeatPassword" className="form-control" />
+              <label className="form-label" htmlFor="registerRepeatPassword">Repetir senha</label>
+            </div>
+            
+            {/* Checkbox de termos */}
+            <div className="form-check d-flex justify-content-center mb-4">
+              <input
+                className="form-check-input me-2"
+                type="checkbox"
+                value=""
+                id="registerCheck"
+                defaultChecked
+                aria-describedby="registerCheckHelpText"
+              />
+              <label className="form-check-label" htmlFor="registerCheck">
+                Eu li e concordo com os termos
+              </label>
+            </div>
+            
+            <button type="submit" className="btn btn-primary btn-block mb-3">
+              Cadastrar
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   </div>
 </div>
