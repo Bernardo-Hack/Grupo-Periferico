04a135312ebc538d8825fbc1db75c71841f48d40
TheoRavaglia <theo.ravaglia@pucpr.edu.br>
2025-05-12 13:58:53 -0300

    feat: finaliza configuração do proxy no vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// No arquivo vite.config.js do frontend
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001', // Porta do backend
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+  }
 })
