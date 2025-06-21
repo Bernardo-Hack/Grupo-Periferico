2537757887bc3503bb501c2c8b92fc90d97cb523
Bernardo de Lima Hack <102930809+Bernardo-Hack@users.noreply.github.com>
2025-06-21 19:21:57 -0300

    Update 01_tabelas.sql

--- db/01_tabelas.sql
+++ db/01_tabelas.sql
@@ -2,7 +2,7 @@
 -- Tabelas
 -- ===========================
 
-CREATE TABLE Usuario (
+CREATE TABLE IF NOT EXISTS Usuario (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     cpf VARCHAR(14) UNIQUE NOT NULL,
@@ -12,23 +12,35 @@ CREATE TABLE Usuario (
     data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE Administrador (
+CREATE TABLE IF NOT EXISTS Administrador (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
+    cpf VARCHAR(14) UNIQUE NOT NULL,
+    telefone VARCHAR(20),
     senha_hash VARCHAR(255) NOT NULL,
+    data_nascimento DATE NOT NULL,
     data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE DoacaoDinheiro (
+CREATE TABLE IF NOT EXISTS Voluntario (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    disponibilidade VARCHAR(255) NOT NULL,
+    experiencia TEXT,
+    data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS DoacaoDinheiro (
     id SERIAL PRIMARY KEY,
     usuario_id INT NOT NULL,
     valor DECIMAL(10,2) NOT NULL,
-    data_doacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_doacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     metodo_pagamento VARCHAR(10) NOT NULL CHECK (metodo_pagamento IN ('pix', 'cartao', 'boleto')),
     FOREIGN KEY (usuario_id) REFERENCES Usuario(id)
 );
 
-CREATE TABLE DoacaoRoupa (
+CREATE TABLE IF NOT EXISTS DoacaoRoupa (
     id SERIAL PRIMARY KEY,
     usuario_id INT NOT NULL,
     tipo VARCHAR(50) NOT NULL,
@@ -38,7 +50,7 @@ CREATE TABLE DoacaoRoupa (
     FOREIGN KEY (usuario_id) REFERENCES Usuario(id)
 );
 
-CREATE TABLE DoacaoAlimento (
+CREATE TABLE IF NOT EXISTS DoacaoAlimento (
     id SERIAL PRIMARY KEY,
     usuario_id INT NOT NULL,
     tipo VARCHAR(50) NOT NULL,
@@ -47,18 +59,7 @@ CREATE TABLE DoacaoAlimento (
     FOREIGN KEY (usuario_id) REFERENCES Usuario(id)
 );
 
-CREATE TABLE Distribuicao (
-    id SERIAL PRIMARY KEY,
-    doacao_id INT NOT NULL,
-    tipo_doacao VARCHAR(10) NOT NULL CHECK (tipo_doacao IN ('dinheiro', 'roupa', 'alimento')),
-    data_distribuicao DATE NOT NULL,
-    admin_id INT NOT NULL,
-    usuario_voluntario_id INT,
-    FOREIGN KEY (admin_id) REFERENCES Administrador(id),
-    FOREIGN KEY (usuario_voluntario_id) REFERENCES Usuario(id)
-);
-
-CREATE TABLE Certificado (
+CREATE TABLE IF NOT EXISTS Certificado (
     id SERIAL PRIMARY KEY,
     usuario_id INT NOT NULL,
     distribuicao_id INT NOT NULL,
